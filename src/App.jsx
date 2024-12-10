66e75178445abb392dacc2c3fb6a6182b6849422
Kyler Berg <kylermberg@gmail.com>
2024-12-10 16:34:41 -0700

    basics added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>My Startup App</h1>
+            <p>This is the starting point for my React application.</p>
+        </div>
+    );
+}
+
+export default App;
